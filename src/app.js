60ac3420f657314608b87fd044f7efa735447f1d
Conor Walsh <conorwalsh0703@gmail.com>
2020-04-06 19:30:52 +0100

    added remove post functionality

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import {
 document.addEventListener('DOMContentLoaded', getPosts)
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost)
+// Listen for delete post
+document.querySelector('#posts').addEventListener('click', deletePost)
 
 function getPosts() {
   http
@@ -35,4 +37,20 @@ function submitPost() {
     })
     .catch(err => console.log(err))
 
+}
+
+function deletePost(e) {
+  e.preventDefault()
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'danger')
+          getPosts()
+        })
+        .catch(err => console.log(err))
+    }
+  }
 }
