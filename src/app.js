9480f8349dfdff52cd1ffad4016775e302fc1cda
Conor Walsh <conorwalsh0703@gmail.com>
2020-04-06 18:56:29 +0100

    displayed posts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
-const greeting = 'Hello World';
-console.log(greeting);
+import {
+  http
+} from './http'
+import {
+  ui
+} from './ui'
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
+// Get posts on DOM load
 
-getData('https://jsonplaceholder.typicode.com/posts');
+document.addEventListener('DOMContentLoaded', getPosts)
+
+function getPosts() {
+  http
+    .get('http://localhost:3000/posts')
+    .then(posts => ui.showPosts(posts))
+    .catch(err => console.log(err))
+}
